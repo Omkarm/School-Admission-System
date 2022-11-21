cc297c0c125fb2ac04a9e0370455a9cf6a6c62d9
Omkar More <msomkar@gitl.com>
2022-11-21 23:36:01 +0530

    third Commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 // import "@fortawesome/fontawesome-free/css/all.min.css";
 // import "bootstrap-css-only/css/bootstrap.min.css";
 // import "mdbreact/dist/css/mdb.css";
-// dqaaaaa
+// dqaaaaas
 
 import Navbar from "./Pages/Components/Navbar";
 // import Footer from "./Pages/Components/Footer";
